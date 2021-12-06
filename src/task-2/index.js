f0a97c0a7736bcad87f1635d0e0e01aec127b8f6
TheHomeGera <suchkovgera12@gmail.com>
2021-12-06 19:21:33 +0500

    случайно не запулились джс файлс

---
+++ src/task-2/index.js
@@ -0,0 +1,22 @@
+"use strict";
+/** Задача 2
+ * Необходимо реализовать асинхронную подгрузку модулей в браузере:
+ * 1) Разнести классы SettingValue, Setting и Example по разным *.ts файлам
+ * 2) После компиляции TypeScript должно получаться 3 файла формата *.js.
+ * 3) Создать index.html, в котором необходимо указать ссылку на первый JavaScript файл,
+ * 	  все остальные файлы должны подгрузиться асинхронно по цепочке, используя requireJS.
+ */
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+exports.__esModule = true;
+__exportStar(require("./SettingValue"), exports);
+__exportStar(require("./Example"), exports);
+__exportStar(require("./Setting"), exports);
