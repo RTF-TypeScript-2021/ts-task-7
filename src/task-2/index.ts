d6e574daacdf307ab8c17266d82e630ca10341b1
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-12-06 19:09:07 +0500

    Task 2 is solved

--- src/task-2/index.ts
+++ src/task-2/index.ts
@@ -5,34 +5,10 @@
  * 3) Создать index.html, в котором необходимо указать ссылку на первый JavaScript файл,
  * 	  все остальные файлы должны подгрузиться асинхронно по цепочке, используя requireJS.
  */
+import * as Lib from "./Lib/Lib";
 
-class SettingValue {
-    public property1: string;
-    public property2: string;
+const setVal = new Lib.SettingValue("firstParam", "secondParam");
+const set = new Lib.Setting("Setting", setVal);
+const ex = new Lib.Example(set);
 
-    constructor(p1: string, p2: string) {
-        this.property1 = p1;
-        this.property2 = p2;
-    }
-}
-
-class Setting {
-    public key: string;
-    public value: SettingValue;
-
-    constructor(k: string, ov: SettingValue) {
-        this.key = k;
-        this.value = ov;
-    }
-}
-
-
-class Example {
-    public title: string;
-    public id: number;
-    private _setting: Setting;
-
-    constructor(s: Setting) {
-        this._setting = s;
-    }
-}
+console.log(ex);
