c9b6aa8acf3edbbccc223455d91965f25365e7a4
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-12-06 19:00:29 +0500

    Task 1 is solved

---
+++ src/task-1/Types/typing.d.ts
@@ -0,0 +1,8 @@
+export declare namespace Custom {
+    interface IUser {
+        age: number;
+        name: string;
+        upAgeByYear(): void;
+    }
+    export let user: IUser;
+}
---
+++ src/task-1/app.ts
@@ -0,0 +1,5 @@
+import {Custom} from "./Types/typing";
+
+console.log(Custom.user.age, Custom.user.name);
+Custom.user.upAgeByYear();
+console.log(Custom.user);
