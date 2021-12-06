bc7651bd975f03e9ca2e3a8b6ed0062905663789
Kaigarodov Andrey <kaigarodov2012@yandex.ru>
2021-12-06 22:24:28 +0500

    add start script, fix task-2

--- src/task-2/app.ts
+++ src/task-2/app.ts
@@ -1,5 +1,6 @@
-import * as SE from './lib'
-import { Setting } from './lib';
+//не понимаю в чем проблема, при использовании ./lib браузер запрашивает файл lib.js,
+//а не файлы, которые в нем экспортируются
+import * as SE from './lib/index'
 
 const settingVal = new SE.SettingValue("100px","200px");
 const settings = new SE.Setting("key", settingVal);
