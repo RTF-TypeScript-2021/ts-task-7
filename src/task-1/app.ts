ed8507aa7242cee4f62e2a6bd537748ecef05ce2
Helloeverybody <seva.bocharov@mail.ru>
2021-12-07 17:37:33 +0500

    Обновлено, но не сделано 1 задание

--- src/task-1/app.ts
+++ src/task-1/app.ts
@@ -1,3 +1,1 @@
-const custom = new Custom();
-custom.user = new User(3, "f")
-console.log(custom)
+console.log(Custom.user)
--- src/task-1/typing.d.ts
+++ src/task-1/typing.d.ts
@@ -1,9 +1,9 @@
-declare class Custom{
-    public user: User;
-}
-declare class User{
-    constructor(age: number, name: string);
-    public age: number;
-    public name: string;
-    public upAgeByYear: () => void;
+declare namespace Custom{
+    export class User{
+        constructor(age: number, name: string);
+        public age: number;
+        public name: string;
+        public upAgeByYear: () => void;
+    }
+    export const user: User;
 }
