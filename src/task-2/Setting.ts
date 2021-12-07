50b45ee9f58adc56af5241c82cf148e3c79e5690
vtyric <misha.torno@gmail.com>
2021-12-07 09:09:19 +0500

    решил второй таск.

---
+++ src/task-2/Setting.ts
@@ -0,0 +1,11 @@
+import {SettingValue} from "./SettingValue";
+
+export class Setting {
+    public key: string;
+    public value: SettingValue;
+
+    constructor(k: string, ov: SettingValue) {
+        this.key = k;
+        this.value = ov;
+    }
+}
--- src/task-2/index.js
+++ src/task-2/index.js
@@ -1,3 +1,4 @@
+"use strict";
 /** Задача 2
  * Необходимо реализовать асинхронную подгрузку модулей в браузере:
  * 1) Разнести классы SettingValue, Setting и Example по разным *.ts файлам
@@ -5,13 +6,11 @@
  * 3) Создать index.html, в котором необходимо указать ссылку на первый JavaScript файл,
  *      все остальные файлы должны подгрузиться асинхронно по цепочке, используя requireJS.
  */
-
-import {SettingValue} from "./SettingValue";
-import {Setting} from "./Setting";
-import {Example} from "./Example";
-
-const settingValue = new SettingValue("p1", "p2");
-const setting = new Setting("k", settingValue);
-const example = new Example(setting);
-
+exports.__esModule = true;
+var SettingValue_1 = require("./SettingValue");
+var Setting_1 = require("./Setting");
+var Example_1 = require("./Example");
+var settingValue = new SettingValue_1.SettingValue("p1", "p2");
+var setting = new Setting_1.Setting("k", settingValue);
+var example = new Example_1.Example(setting);
 console.log(example);
