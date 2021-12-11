6a6e7a90888c87e21192f8e5ee1c41c2a7f22618
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-12-11 17:31:09 +0500

    Task 1 is solved

--- src/task-1/app.ts
+++ src/task-1/app.ts
@@ -1,5 +1,5 @@
-import {Custom} from "./Types/typing";
-
-console.log(Custom.user.age, Custom.user.name);
-Custom.user.upAgeByYear();
-console.log(Custom.user);
+namespace Custom {
+    console.log(user.age, user.name);
+    user.upAgeByYear();
+    console.log(user);
+}
